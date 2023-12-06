ee0e806e42e33d72e2a839065c04cc8d6d5832d6
Nicolás Faure <nfaure@insideone.cloud>
2023-12-05 23:22:55 -0300

    feat: feature/userSchemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RegisterModule]
+  imports: [
+    AuthModule,
+    UsersModule,
+    RegisterModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/box-mongodb')
+  ]
 })
 export class AppModule {}
