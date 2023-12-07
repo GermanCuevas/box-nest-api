214ad33c237f170aa6ced1741a13bf2130f2e3ba
flormartinez92 <martinez.mf.92@gmail.com>
2023-12-07 17:56:06 -0300

    fix: fix develop

--- config/schemas/user.schema.ts
+++ config/schemas/user.schema.ts
@@ -1,26 +1,26 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { HydratedDocument } from 'mongoose';
 
-export type UserDocument = HydratedDocument<User>;
+// export type UserDocument = HydratedDocument<User>;
 
-@Schema()
-export class User {
-  @Prop({ required: true })
-  name: string;
-  @Prop({ required: true })
-  lastname: string;
-  @Prop({ required: true })
-  password: string;
-  @Prop({ required: true })
-  email: string;
-  // @Prop()
-  // coordinates: Object;
-  @Prop()
-  imgAvatar: string;
-  @Prop({ required: true })
-  isDisabled: boolean;
-  @Prop({ required: true })
-  isAdmin: boolean;
-}
+// @Schema()
+// export class User {
+//   @Prop({ required: true })
+//   name: string;
+//   @Prop({ required: true })
+//   lastname: string;
+//   @Prop({ required: true })
+//   password: string;
+//   @Prop({ required: true })
+//   email: string;
+//   // @Prop()
+//   // coordinates: Object;
+//   @Prop()
+//   imgAvatar: string;
+//   @Prop({ required: true })
+//   isDisabled: boolean;
+//   @Prop({ required: true })
+//   isAdmin: boolean;
+// }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+// export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,24 @@
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
-import { User } from 'config/schemas/user.schema';
+
 // This should be a real class/interface representing a user entity
-export type IUser = any;
+export type User = any;
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme'
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess'
+    }
+  ];
 
-  async create(createUserDto: any): Promise<User> {
-    const createdUser = await this.UserModel.create(createUserDto);
-    return createdUser.save();
-  }
-
-  async findOne(username: string): Promise<IUser | undefined> {
-    return await this.UserModel.find((user: any) => user.username === username);
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
   }
 }
