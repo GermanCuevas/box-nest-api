db71df23b298aed7e3df874f84272569a0ce2f0e
flormartinez92 <martinez.mf.92@gmail.com>
2023-12-13 16:49:31 -0300

    feat: testing

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from './entities/users.entity';
+import { getModelToken } from '@nestjs/mongoose';
+
+// Define la clase JwtServiceMock
+class JwtServiceMock {
+  // Implementa las funciones necesarias para tus pruebas
+  signAsync(payload: any): string {
+    return 'token';
+  }
+}
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController]
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        { provide: JwtService, useClass: JwtServiceMock },
+        { provide: getModelToken(User.name), useValue: {} }
+      ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
