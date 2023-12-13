af3ce1bf0818506887a062c93be6173edc609108
Nicolás Faure <nfaure@insideone.cloud>
2023-12-13 15:32:23 -0300

    feat: test auth

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { getModelToken } from '@nestjs/mongoose'; // Asegúrate de importar el token correcto
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('rutas de autenticación', () => {
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: getModelToken('NombreDeTuModelo'), // Reemplaza 'NombreDeTuModelo' con el nombre real de tu modelo
+          useValue: {} // Puedes proporcionar un objeto de modelo vacío o un objeto de modelo simulado aquí
+        }
+      ]
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = moduleRef.get<AuthService>(AuthService);
+    authController = moduleRef.get<AuthController>(AuthController);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('debería devolver un usuario y establecer una cookie', async () => {
+      const usuarioMock = {
+        _id: 'id_usuario_mock',
+        name: 'NombreUsuario',
+        email: 'correo@ejemplo.com',
+        checkpass: jest.fn() // Puedes simular el comportamiento de checkpass si es necesario
+      };
+
+      jest.spyOn(authService, 'loginUser').mockImplementation(async () => {
+        return usuarioMock;
+      });
+
+      // Puedes simular el comportamiento de findOne según tus necesidades
+      jest.spyOn(authService['userModel'], 'findOne').mockImplementation(async () => usuarioMock);
+
+      const respuesta = await authController.findOne();
+      expect(respuesta).toBe(usuarioMock);
+
+      // Asegúrate de que se llamó a checkpass con la contraseña correcta
+      expect(usuarioMock.checkpass).toHaveBeenCalledWith('contraseña_correcta');
+    });
   });
 });
